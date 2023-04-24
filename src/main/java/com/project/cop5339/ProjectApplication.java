687937e3ff9cb2df4efbccc0384e600631d4aab6
DESKTOP-K2F6M1C\A_mah <amahmood561@gmail.com>
2023-04-23 20:47:14 -0400

    update for prototype seller screen

--- src/main/java/com/project/cop5339/ProjectApplication.java
+++ src/main/java/com/project/cop5339/ProjectApplication.java
@@ -1,5 +1,7 @@
 package com.project.cop5339;
 
+import com.project.cop5339.controller.SellerController;
+import com.project.cop5339.swing.SellerScreen;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -12,7 +14,10 @@ public class ProjectApplication {
 
         //SpringApplication.run(ProjectApplication.class, args);
         ConfigurableApplicationContext context = SpringApplication.run(ProjectApplication.class, args);
-
+        // Launch the SellerScreen
+        SellerController sellerController = context.getBean(SellerController.class);
+        SellerScreen sellerScreen = new SellerScreen();
+        sellerScreen.setVisible(true);
         // Launch the Swing application
         //HelloWorldController controller = context.getBean(HelloWorldController.class);
        //         HelloWorldSwing helloWorldSwing = new HelloWorldSwing(controller);
